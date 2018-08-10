10d8b277651480ce0616b7f0247b3fce4ae46311
coco <chenyuanfang@chenyuanfangdeMacBook-Pro.local>
2018-08-10 13:38:46 +0800

    add

---
+++ CEngine/src/CMakeFiles/CEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CEngine.dir/main.cpp.o"
+  "CEngine.pdb"
+  "CEngine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
